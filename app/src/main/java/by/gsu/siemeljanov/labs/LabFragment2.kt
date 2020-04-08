b470f1478fcf1274238c30e132c9f628c55ffc19
EmelyanovSI <xemelyanov@gmail.com>
2020-04-08 12:14:30 +0300

    android - lab2 - animations

--- app/src/main/java/by/gsu/siemeljanov/labs/LabFragment2.kt
+++ app/src/main/java/by/gsu/siemeljanov/labs/LabFragment2.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import by.gsu.siemeljanov.R
 import by.gsu.siemeljanov.model.ListItem
 import kotlinx.android.synthetic.main.fragment_lab2.*
@@ -25,6 +26,21 @@ class LabFragment2(private val lab: ListItem) : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        text.text = lab.title
+        text1.text = lab.title
+        text2.text = getString(R.string.laboratory_work)
+
+        val zoom = AnimationUtils.loadAnimation(context, R.anim.zoom)
+        val clockwise = AnimationUtils.loadAnimation(context, R.anim.clockwise)
+        val fade = AnimationUtils.loadAnimation(context, R.anim.fade)
+        val blink = AnimationUtils.loadAnimation(context, R.anim.blink)
+        val move = AnimationUtils.loadAnimation(context, R.anim.move)
+        val slide = AnimationUtils.loadAnimation(context, R.anim.slide)
+
+        button1.setOnClickListener { imageView.startAnimation(zoom) }
+        button2.setOnClickListener { imageView.startAnimation(clockwise) }
+        button3.setOnClickListener { imageView.startAnimation(fade) }
+        button4.setOnClickListener { imageView.startAnimation(blink) }
+        button5.setOnClickListener { imageView.startAnimation(move) }
+        button6.setOnClickListener { imageView.startAnimation(slide) }
     }
 }
