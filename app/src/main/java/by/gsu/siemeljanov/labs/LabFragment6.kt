91e12cd965e74a8364494f0aa0e9cefe4d679aeb
EmelyanovSI <xemelyanov@gmail.com>
2020-04-09 12:29:53 +0300

    android - lab6 - best practices

--- app/src/main/java/by/gsu/siemeljanov/labs/LabFragment6.kt
+++ app/src/main/java/by/gsu/siemeljanov/labs/LabFragment6.kt
@@ -1,10 +1,14 @@
 package by.gsu.siemeljanov.labs
 
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.BatteryManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import by.gsu.siemeljanov.R
 import by.gsu.siemeljanov.model.ListItem
 import kotlinx.android.synthetic.main.fragment_lab6.*
@@ -25,6 +29,31 @@ class LabFragment6(private val lab: ListItem) : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        text.text = lab.title
+        text1.text = lab.title
+        materialButton1.setOnClickListener {
+            val filter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+            val batteryStatus = context?.registerReceiver(null, filter)
+
+            val status = batteryStatus?.getIntExtra(BatteryManager.EXTRA_STATUS, -1)
+            val isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING
+                    || status == BatteryManager.BATTERY_STATUS_FULL
+
+            val chargePlug = batteryStatus?.getIntExtra(BatteryManager.EXTRA_PLUGGED, -1)
+            val usbCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_USB
+            val acCharge = chargePlug == BatteryManager.BATTERY_PLUGGED_AC
+
+            if (isCharging) {
+                when (isCharging) {
+                    usbCharge -> toast("${getString(R.string.charging_on)} ${getString(R.string.usb)}")
+                    acCharge -> toast("${getString(R.string.charging_on)} ${getString(R.string.ac)}")
+                }
+            } else {
+                toast(getString(R.string.no_charge))
+            }
+        }
+    }
+
+    private fun toast(string: String) {
+        Toast.makeText(context, string, Toast.LENGTH_SHORT).show()
     }
 }
