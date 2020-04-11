1d68e09053ecdcdeca7da924c9fbc2807be3ada7
EmelyanovSI <xemelyanov@gmail.com>
2020-04-11 18:55:29 +0300

    android - lab9 - clipboard

---
+++ app/src/main/java/by/gsu/siemeljanov/labs/lab9/CopyPasteModel.kt
@@ -0,0 +1,8 @@
+package by.gsu.siemeljanov.labs.lab9
+
+import androidx.databinding.ObservableField
+
+class CopyPasteModel {
+    var copy = ObservableField<String>()
+    var paste = ObservableField<String>()
+}
