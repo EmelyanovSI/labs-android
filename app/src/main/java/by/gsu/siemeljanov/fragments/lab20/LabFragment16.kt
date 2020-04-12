0f78c604e1afef7a7ef5675bdf991afa43d22cd3
EmelyanovSI <xemelyanov@gmail.com>
2020-04-12 19:31:14 +0300

    android - lab16 - image switcher

--- app/src/main/java/by/gsu/siemeljanov/fragments/lab20/LabFragment16.kt
+++ app/src/main/java/by/gsu/siemeljanov/fragments/lab20/LabFragment16.kt
@@ -1,10 +1,13 @@
 package by.gsu.siemeljanov.fragments.lab20
 
+import android.app.ActionBar
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
+import android.widget.ImageView
 import by.gsu.siemeljanov.R
 import by.gsu.siemeljanov.model.ListItem
 import kotlinx.android.synthetic.main.fragment_lab16.*
@@ -26,6 +29,23 @@ class LabFragment16(private val lab: ListItem) : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         text1.text = lab.title
+        imageSwitcher.setFactory {
+            val myView = ImageView(context)
+            myView.scaleType = ImageView.ScaleType.FIT_CENTER
+            myView.layoutParams = FrameLayout.LayoutParams(
+                ActionBar.LayoutParams.WRAP_CONTENT,
+                ActionBar.LayoutParams.WRAP_CONTENT
+
+            )
+            myView
+        }
+        imageSwitcher.setImageResource(R.drawable.code)
+        materialButton1.setOnClickListener {
+            imageSwitcher.setImageResource(R.drawable.code)
+        }
+        materialButton2.setOnClickListener {
+            imageSwitcher.setImageResource(R.drawable.schedule)
+        }
     }
 
 }
