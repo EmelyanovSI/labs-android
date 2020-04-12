fec9573a47b467c538ae6894a0338e5a610090ff
EmelyanovSI <xemelyanov@gmail.com>
2020-04-12 16:09:39 +0300

    android - lab14 - google maps

--- app/src/main/java/by/gsu/siemeljanov/fragments/lab20/LabFragment14.kt
+++ app/src/main/java/by/gsu/siemeljanov/fragments/lab20/LabFragment14.kt
@@ -1,15 +1,19 @@
 package by.gsu.siemeljanov.fragments.lab20
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import by.gsu.siemeljanov.MapsActivity
 import by.gsu.siemeljanov.R
 import by.gsu.siemeljanov.model.ListItem
 import kotlinx.android.synthetic.main.fragment_lab14.*
 
 class LabFragment14(private val lab: ListItem) : Fragment() {
+
+    val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +30,11 @@ class LabFragment14(private val lab: ListItem) : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         text1.text = lab.title
+        materialButton.setOnClickListener {
+            val intent = Intent(activity, MapsActivity::class.java).apply {
+                putExtra(EXTRA_MESSAGE, lab.title)
+            }
+            startActivity(intent)
+        }
     }
-
 }
