6d9dbe7884be54a145533deb78ad55f02b4a9aec
EmelyanovSI <xemelyanov@gmail.com>
2020-04-13 02:20:46 +0300

    android - lab22 - login screen

--- app/src/main/java/by/gsu/siemeljanov/fragments/lab30/LabFragment22.kt
+++ app/src/main/java/by/gsu/siemeljanov/fragments/lab30/LabFragment22.kt
@@ -5,14 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ObservableField
 import by.gsu.siemeljanov.R
+import by.gsu.siemeljanov.databinding.FragmentLab22Binding
 import by.gsu.siemeljanov.model.ListItem
 import kotlinx.android.synthetic.main.fragment_lab22.*
 
 class LabFragment22(private val lab: ListItem) : Fragment() {
 
+    private val admin = "admin"
+    private var counter = 3
+    var counterString = ObservableField<String>()
+    var login = ObservableField<String>()
+    var password = ObservableField<String>()
+    private lateinit var binding: FragmentLab22Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        counterString.set(counter.toString())
         retainInstance = true
     }
 
@@ -20,12 +32,32 @@ class LabFragment22(private val lab: ListItem) : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_lab22, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_lab22, container, false)
+        binding.user = this
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         text1.text = lab.title
+        text4.visibility = View.GONE
+        materialButton1.setOnClickListener {
+            if (login.get() == admin && password.get() == admin) {
+                Toast.makeText(context, R.string.text_redirecting, Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(context, R.string.wrong_credentials, Toast.LENGTH_SHORT).show()
+                text4.visibility = View.VISIBLE
+                --counter
+                counterString.set(counter.toString())
+                if (counter == 0) {
+                    materialButton1.isEnabled = false
+                }
+            }
+        }
+        materialButton2.setOnClickListener {
+            login.set("")
+            password.set("")
+        }
     }
 
 }
