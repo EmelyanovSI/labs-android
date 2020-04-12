7bfba315db90f025e531b83159cf2be6471c5b8a
EmelyanovSI <xemelyanov@gmail.com>
2020-04-12 14:19:54 +0300

    android - lab13 - gestures

--- app/src/main/java/by/gsu/siemeljanov/fragments/lab20/LabFragment13.kt
+++ app/src/main/java/by/gsu/siemeljanov/fragments/lab20/LabFragment13.kt
@@ -1,15 +1,21 @@
 package by.gsu.siemeljanov.fragments.lab20
 
+import android.graphics.Matrix
 import android.os.Bundle
+import android.view.*
+import android.view.ScaleGestureDetector.SimpleOnScaleGestureListener
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import by.gsu.siemeljanov.R
 import by.gsu.siemeljanov.model.ListItem
 import kotlinx.android.synthetic.main.fragment_lab13.*
+import kotlin.math.max
+import kotlin.math.min
 
 class LabFragment13(private val lab: ListItem) : Fragment() {
+
+    private var matrix = Matrix()
+    private var scale = 1f
+    private lateinit var SGD: ScaleGestureDetector
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +26,15 @@ class LabFragment13(private val lab: ListItem) : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_lab13, container, false)
+        val view = inflater.inflate(R.layout.fragment_lab13, container, false)
+        SGD = ScaleGestureDetector(context, ScaleListener())
+        view.setOnTouchListener { _, motionEvent ->
+            if (motionEvent.action == MotionEvent.ACTION_MOVE) {
+                SGD.onTouchEvent(motionEvent)
+            }
+            true
+        }
+        return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -28,4 +42,13 @@ class LabFragment13(private val lab: ListItem) : Fragment() {
         text1.text = lab.title
     }
 
+    private inner class ScaleListener : SimpleOnScaleGestureListener() {
+        override fun onScale(detector: ScaleGestureDetector): Boolean {
+            scale *= detector.scaleFactor
+            scale = max(0.1f, min(scale, 5.0f))
+            matrix.setScale(scale, scale)
+            imageView.imageMatrix = matrix
+            return true
+        }
+    }
 }
